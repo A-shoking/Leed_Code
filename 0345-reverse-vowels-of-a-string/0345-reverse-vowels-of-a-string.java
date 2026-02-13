9e39b682e93f731bc0f1cfa6082b8f43bbeecedb
Ashok <ashokkumargo190@gmail.com>
2026-02-13 16:58:48 -0600

    Time: 9 ms (10.82%), Space: 47.1 MB (17.28%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,47 +1,35 @@
 class Solution {
-    public String reverseVowels(String s) {   
-         HashSet<Character> vowels = new HashSet<>();
-        vowels.add('a');
-        vowels.add('e');
-        vowels.add('i');
-        vowels.add('o');
-        vowels.add('u');
-        vowels.add('A');
-        vowels.add('E');
-        vowels.add('I');
-        vowels.add('O');
-        vowels.add('U');
+    public String reverseVowels(String s) {
+        Set<Character> keys = new HashSet<>();
+        keys.add('a');
+        keys.add('e');
+        keys.add('i');
+        keys.add('o');
+        keys.add('u');
+       
+        int left = 0;
+        int right = s.length()-1;
+       System.out.println(right);
 
-        // Convert string to a character array since strings are immutable
-        char[] chars = s.toCharArray();
-        int start = 0;
-        int last = chars.length - 1;
-
-        // Two-pointer approach
-        while (start < last) {
-            // Move start pointer until it points to a vowel
-           
-             if(!vowels.contains(chars[start])){
-                    start++;
-                }
-            // Move last pointer until it points to a vowel
-             if(!vowels.contains(chars[last])){
-                     last--;
-                }
-            if( vowels.contains(chars[start]) && vowels.contains(chars[last]) ){
-            // Swap vowels
-                char temp = chars[start];
-                chars[start] = chars[last];
-                chars[last] = temp;
-
-                // Move pointers
-                start++;
-                last--;
-            }
+    StringBuffer sb = new StringBuffer(s);
+       while (left < right) {
+        char left_vowel = s.charAt(left);
+        char right_vowel = s.charAt(right);
+        if (!keys.contains( Character.toLowerCase(left_vowel))) {
+            left++;
+            continue;
+        } 
+        if (!keys.contains(Character.toLowerCase(right_vowel))) {
+            right--;
+            continue;
         }
+        sb.setCharAt(left, right_vowel);
+        sb.setCharAt(right, left_vowel);
+        left++;
+        right--;
 
-        // Convert character array back to string
-        return new String(chars);
-        
+       }
+
+        return sb.toString();
     }
 }
