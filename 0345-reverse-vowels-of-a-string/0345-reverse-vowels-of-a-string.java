7952188644254054944a362259f8883bcdf06afb
Ashok <ashokkumargo190@gmail.com>
2026-05-07 21:54:37 -0500

    Time: 6 ms (18.61%), Space: 47.1 MB (13.95%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,35 +1,25 @@
 class Solution {
     public String reverseVowels(String s) {
-        Set<Character> keys = new HashSet<>();
-        keys.add('a');
-        keys.add('e');
-        keys.add('i');
-        keys.add('o');
-        keys.add('u');
-       
-        int left = 0;
-        int right = s.length()-1;
-       System.out.println(right);
+      Set<Character> st = new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
 
-    StringBuffer sb = new StringBuffer(s);
-       while (left < right) {
-        char left_vowel = s.charAt(left);
-        char right_vowel = s.charAt(right);
-        if (!keys.contains( Character.toLowerCase(left_vowel))) {
+      int left = 0;
+      int right = s.length()-1;
+      StringBuilder sb = new StringBuilder(s);
+
+      while(left < right) {
+        if(!st.contains(s.charAt(left))) {
             left++;
-            continue;
-        } 
-        if (!keys.contains(Character.toLowerCase(right_vowel))) {
+        }else if(!st.contains(s.charAt(right))){
             right--;
-            continue;
+        }else {
+            char temp = s.charAt(right);
+            sb.setCharAt(right,s.charAt(left));
+           sb.setCharAt(left,temp);
+           left++;
+           right--;
+
         }
-        sb.setCharAt(left, right_vowel);
-        sb.setCharAt(right, left_vowel);
-        left++;
-        right--;
-
-       }
-
-        return sb.toString();
+      }
+      return sb.toString();
     }
 }
